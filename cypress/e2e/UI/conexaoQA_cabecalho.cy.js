1e1690398b308311336b3a418af7faf5d420c09b
oliveiraericab <oliveiraericab@gmail.com>
2023-06-19 21:32:23 -0300

    adicionado testes de cabeçalho utilizando array

--- cypress/e2e/UI/conexaoQA_cabecalho.cy.js
+++ cypress/e2e/UI/conexaoQA_cabecalho.cy.js
@@ -24,7 +24,7 @@ describe('cabeçalho da página home', () => {
         })
 
         it('valida o menu Cadastrar', () => {
-            cy.get('[data-test=navbar-register ]')
+            cy.get('[data-test=navbar-register]')
                 .should('have.text', 'Cadastrar')
                 .and('have.attr', 'href', '/cadastrar')
                 .and('not.have.attr', 'target')
@@ -35,6 +35,40 @@ describe('cabeçalho da página home', () => {
                 .should('have.text', 'Login')
                 .and('have.attr', 'href', '/login')
                 .and('not.have.attr', 'target')
+        })
+    
+
+        it('valida os menus utilizando array', () => {
+
+            const menus = [
+                { seletor: 'navbar-conexaoQA', link: '/' },
+                { seletor: 'navbar-QAs', link: '/perfis' },
+                { seletor: 'navbar-about', link: '/sobre' },
+                { seletor: 'navbar-register', link: '/cadastrar' },
+                { seletor: 'navbar-login', link: '/login' }
+            ]
+
+            menus.forEach(({ seletor, link }) => {
+                cy.get(`[data-test=${seletor}]`)
+                    .should('have.attr', 'href', link)
+                    .and('not.have.attr', 'target')
+            })
+        })
+
+        ;[
+            { seletor: 'navbar-conexaoQA', link: '/', menu: ' ConexãoQA' },
+            { seletor: 'navbar-QAs', link: '/perfis', menu: 'QAs' },
+            { seletor: 'navbar-about', link: '/sobre', menu: 'Sobre' },
+            { seletor: 'navbar-register', link: '/cadastrar', menu: 'Cadastrar' },
+            { seletor: 'navbar-login', link: '/login', menu: 'Login' }
+        ].forEach(({ seletor, link, menu }) => {
+
+            it.only(`valida o menu ${menu}`, () => {
+                cy.get(`[data-test=${seletor}]`)
+                    .should('have.attr', 'href', link)
+                    .and('not.have.attr', 'target')
+                    .and('have.text', menu)
+            })
         })
     })
 
